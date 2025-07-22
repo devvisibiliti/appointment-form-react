a6f3ae6abfb0c5dcbdf63a8b999bd09b3c1bfd79
devvisibiliti <dev@visibiliti.in>
2025-07-22 15:38:50 +0530

    uopdated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// If using Vitest:
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    outDir: 'dist'  // 👈 Important for Vercel
+  }
 });
